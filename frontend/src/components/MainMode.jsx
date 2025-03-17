56657730d6cd059b1f1f87d9031dae0b5eaa92b9
Haha <haha4ni@hotmail.com>
2025-03-17 22:28:25 +0800

    upload

--- frontend/src/components/MainMode.jsx
+++ frontend/src/components/MainMode.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, CssBaseline, Typography, Button } from "@mui/material";
-import Menu from "./Menu"; // Add this import
+import Menu from "./Menu";
+import SimpleList from "./List";
 
 export default function MainMode({ setMode }) {
   const [state, setState] = useState(null);
@@ -9,19 +10,22 @@ export default function MainMode({ setMode }) {
     // Add your side-effect logic here
   }, []);
 
+  const mainContent = (
+    <Box sx={{ bgcolor: "green", display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', width: '100%' }}>
+      <Typography variant="h4" sx={{ mr: 2 }}>Main Mode</Typography>
+      <Button variant="contained" onClick={() => setMode('book')}>Switch to Book Mode</Button>
+      <Button variant="contained" onClick={() => setMode('book')}>Switch to Book Mode</Button>
+    </Box>
+  );
+
   return (
     <div>
       <CssBaseline />
       <Menu />
 
-
-
-
-      
-      <Box sx={{ p: 20, bgcolor: "green" }}>
-        <Typography variant="h4">Main Mode</Typography>
-        {/* Add your component JSX here */}
-        <Button variant="contained" onClick={() => setMode('book')}>Switch to Book Mode</Button>
+      <Box sx={{ display: 'flex', pt: 5 }}>
+      <SimpleList />
+      {mainContent}
       </Box>
     </div>
   );
